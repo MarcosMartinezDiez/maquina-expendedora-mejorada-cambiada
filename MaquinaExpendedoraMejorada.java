f7fb9c70a3ec1bc1c8dd198fe232b2478fd80315
marcos <martinezdiezmarcos7@gmail.com>
2022-11-21 09:53:41 +0100

    Ahora si no pones el precio necesario salta un mensaje de error con la cantidad de dinero que falta

--- MaquinaExpendedoraMejorada.java
+++ MaquinaExpendedoraMejorada.java
@@ -54,7 +54,8 @@ public class MaquinaExpendedoraMejorada {
      * Imprime un billete para el cliente actual
      */
     public void imprimirBillete() {
-        if (balanceClienteActual >= precioBillete) {        
+        int cantidadDeDineroQueFalta=precioBillete - balanceClienteActual;
+        if (precioBillete < balanceClienteActual) {        
             // Simula la impresion de un billete
             System.out.println("##################");
             System.out.println("# Billete de tren:");
@@ -67,11 +68,12 @@ public class MaquinaExpendedoraMejorada {
             totalDineroAcumulado = totalDineroAcumulado + precioBillete;
             // Reduce el balance del cliente actual dejandole seguir utilizando la maquina
             balanceClienteActual = balanceClienteActual - precioBillete;
-        }
+        }           
         else {
-            System.out.println("Necesitas introducir " + (precioBillete - balanceClienteActual) + " euros mas!");
-                    
-        }            
+            System.out.println("Cantidad De Dinero Que Falta" + cantidadDeDineroQueFalta);
+
+     
+        }
     }
     
     /**
